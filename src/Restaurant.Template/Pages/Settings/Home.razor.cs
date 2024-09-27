4695cb6a687adb21ed5b94cd1d7f73a1cba2bd93
SR Ganesh Ram <sr.ganeshram@hotmail.com>
2024-09-27 21:08:16 +0530

    updates of settings page GUI V 0.0.2

---
+++ src/Restaurant.Template/Pages/Settings/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Restaurant.Template.Pages.Settings;
+
+public partial class Home : ComponentBase
+{
+    async Task NavigateClicked(string navigateTo)
+    {
+        Navigation.NavigateTo($"/settings/{navigateTo}");
+    }
+    protected override void OnInitialized()
+    {
+        Navigation.LocationChanged += HandleLocationChanged;
+    }
+
+    private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        Logger.LogInformation("URL of new location: {Location}", e.Location);
+    }
+
+    public void Dispose()
+    {
+        Navigation.LocationChanged -= HandleLocationChanged;
+    }
+}
